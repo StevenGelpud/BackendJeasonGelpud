75af1432b0e1bd7b3d9c8f14eaad2947979b6fa8
Jeason Gelpud <jeisongelpud@udenar.edu.co>
2024-09-28 18:45:42 -0500

    Commit 2: Pagina principal app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
-import express from "express"
+import express from "express";
+import { routerMascotas } from "./rutas/mascotasRouter.js";
+import {db} from "./database/conexion.js";
+import cors from "cors";
+//Crear instancia de Express
+const app = express();
+//Cors
+app.use(cors());
+//Middleware JSON
+app.use(express.json());
+
+//Verificar Conexion Base Datos
+db.authenticate().then(()=>{
+    console.log(`Conexion a Base de datos correcta`);
+}).catch(err=>{
+    console.log(`Conexion a Base de datos incorrecta ${err}`);
+});
+
+
+//Definir Rutas
+app.get('/', (req, res) => {
+    res.send('Bienvenido al sitio de adopción de mascotas');
+});
+
+//Llamar rutas de mascotas
+app.use("/mascotas",routerMascotas);
+
+//Puerto de Servidor
+const PORT=4000;
+
+// sincronizacion con la base de datos 
+db.sync().then(()=>{
+    //Abri servicio e iniciar el Servidor
+    app.listen(PORT,()=>{
+        console.log(`Servidor Inicializado en el puerto ${PORT}`);
+    })
+
+}).catch(err=>{
+    console.log(`Error al Sincronizar base de datos ${err}`);
+});
+
+
+
+
